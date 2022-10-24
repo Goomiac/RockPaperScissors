5af8758fdc6ebbfd94f23071aa3b4ce931144299
Goomiac <giacomo.campa@hotmail.it>
2022-10-24 18:30:52 +0200

    Add simple UI, add basic UI functionality

--- script.js
+++ script.js
@@ -17,19 +17,16 @@ function getComputerChoice() {
 }
 
 function playRound() {
-    let playerSelection = prompt("Select your move (rock, paper or scissors): ").toLowerCase();
+    let playerSelection = this.id;
     let computerSelection = getComputerChoice();
     console.log("Player selection is: " + playerSelection);
     console.log("Computer selection is: " + computerSelection);
 
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        score += 1;
         return "You WIN! " + playerSelection + " beats " + computerSelection;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        score += 1;
         return "You WIN! " + playerSelection + " beats " + computerSelection;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        score += 1;
         return "You WIN! " + playerSelection + " beats " + computerSelection;
     } else if (playerSelection === computerSelection) {
         return "It's a TIE!";
@@ -38,40 +35,10 @@ function playRound() {
     }
 }
 
-function game() {
-    
-    for (let i = 1; i <= 5; i++) {
-        console.log("Round n." + i);
-        console.log(playRound());
-    }
-    console.log("Game is finished, your score is: " + score + "/5");
-}
+const rock_btn = document.querySelector('#rock');
+const paper_btn = document.querySelector('#paper');
+const scissors_btn = document.querySelector('#scissors');
 
-let score = 0;
-
-game();
-
-/*
-Important note: you want to return the results of this function call, not console.log() them.
-You’re going to use what you return later on, so let’s test this function by using console.log to see the results:
-
-function playRound(playerSelection, computerSelection) {
-  // your code here!
-}
- 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-Write a NEW function called game(). Call the playRound function inside of this one to play a
-5 round game that keeps score and reports a winner or loser at the end.
-Remember loops? This is a great opportunity to use one to play those five rounds:
-
-for (let i = 0; i < 5; i++) {
-   // your code here!
-}
-At this point you should be using console.log() to display the results of each round and the winner at the end.
-Use prompt() to get input from the user. Read the docs here if you need to.
-Feel free to re-work your previous functions if you need to. Specifically, you might want to change
-the return value to something more useful.
-Feel free to create more “helper” functions if you think it would be useful.
-*/
+rock_btn.addEventListener('click', playRound);
+paper_btn.addEventListener('click', playRound);
+scissors_btn.addEventListener('click', playRound);
